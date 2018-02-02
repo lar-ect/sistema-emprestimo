427c77abc32e79069ef035639c365e9b144c2524
Marcus Dantas <marcus.dantas@bct.ect.ufrn.br>
2018-02-02 14:55:08 -0300

    ajuste

--- config/server.js
+++ config/server.js
@@ -41,7 +41,13 @@ app.use(expressSession({
 	resave: false,
 	saveUninitialized: false
 }));
-
+app.use(function(req, res, next){
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
 
 
 /* Efetua o autoload das rotas, dos models e dos controllers para o objeto app */
