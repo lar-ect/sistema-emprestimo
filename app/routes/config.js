bdbaa6c8df852b1c62e6d18cb3fca0849e7aadab
Marcus Dantas <marcus.dantas@bct.ect.ufrn.br>
2018-02-02 13:45:47 -0300

    ajuste

--- app/routes/config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-module.exports = (application) => {
-    application.get("*/*.js", (req, res) => res.download(path.resolve(`./public/dist/${req.params[1]}.js`)));
-    application.get("*/*.css", (req, res) => res.download(path.resolve(`./public/dist/${req.params[1]}.css`)));
-    application.get("*/*.ico", (req, res) => res.download(path.resolve(`./public/dist/${req.params[1]}.ico`)));
-    application.get("*/*.eot", (req, res) => res.download(path.resolve(`./public/dist/${req.params[1]}.eot`)));
-    application.get("*/*.svg", (req, res) => res.download(path.resolve(`./public/dist/${req.params[1]}.svg`)));
-    application.get("*/*.ttf", (req, res) => res.download(path.resolve(`./public/dist/${req.params[1]}.ttf`)));
-}
